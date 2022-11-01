5cf9a80d82eb1026caa844a8a599e8c9f6e3a725
SushreeSurelee <sushreesureleesahu98@gmail.com>
2022-11-01 07:30:40 +0530

    [Sushree | Added Material UI in Registration page]

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-//import Login from './pages/Login/Login';
+import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 
 function App() {
   return (
     <div className="App">
-      {/* <Login></Login> */}
-      <Register></Register>
+      {/* <Login /> */}
+      <Register />
     </div>
   );
 }
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,39 +1,65 @@
 import React from "react";
 import '../Register/Register.css'
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 function Register() {
-    return(
-      <div className="container">
-				<div className="left">
-					<div className="form">
-					 <img src="../FundooLogo.png" ></img>
-           <h3>Create your Fundoo Account</h3>
-					 <form>
-						<input type="text" name="" placeholder="First Name" required></input>
-						<input type="text" name="" placeholder="Last Name" required></input>
+  return(
+   <div className="container">
+		<div className="left">
+		<img src="../FundooLogo.png" alt="Fundoo"></img>
+        <h3>Create your Fundoo Account</h3>
+		<form>
+			<TextField type="text" 
+			sx={{ marginTop:0.7,width: 0.49}} 
+  			InputLabelProps={{style: {fontSize: 13}}} // font size of input label
+			label="First Name"
+			variant="outlined" size="small" required 
+			/>
 
-						<input type="email" name="" placeholder="Username" required></input>
-						<span>You can use letters, numbers & periods</span>
-						<br />
-						<a href="#" >Use my current email address instead</a>
-						<input type="password" name=" " placeholder="Password" required></input>
-						<input type="password" name=" " placeholder="Confirm" required></input>
+			<TextField type="text" 
+			sx={{ marginTop:0.7,marginLeft:0.7, width: 0.49 }} 
+			InputLabelProps={{style: {fontSize: 13}}}
+			label="Last Name" 
+			variant="outlined" size="small" required 
+			/>
 
-						<span>Use 8 or more characters with a mix of letters, numbers & symbols</span>
+			<TextField type="email" 
+			sx={{ marginTop:4, width: 0.99 }} 
+			InputLabelProps={{style: {fontSize: 13}}}
+			label="Username"  size="small" required 
+			/>
+			<span>You can use letters, numbers & periods</span>
+			<br /><br/>
+			<a href="https://www.google.com/" >Use my current email address instead</a>
+			
+            <TextField type="password" 
+			sx={{ marginTop:4, width: 0.49}} 
+ 			InputLabelProps={{style: {fontSize: 13}}}
+			label="Password"
+			variant="outlined" size="small" required 
+			/>
 
-						<div className="bottom-link">
-							<a href="#" >Sign in instead</a>
-							<input type="submit" id="next" value="Next"></input>
-						</div>
-					 </form>
-					</div>
-				</div>
-				<div className="right">
-					<img src="../Sidelogo.PNG"></img>
-					<p>One account. All of Fundoo working for you.</p>
-				</div>
-      </div>
-    )
+		  	<TextField type="password" 
+			sx={{ marginTop:4,marginLeft:0.7, width: 0.49 }} 
+			InputLabelProps={{style: {fontSize: 13}}}
+			label="Confirm" 
+			variant="outlined" size="small" required 
+			/>
+			<span>Use 8 or more characters with a mix of letters, numbers & symbols</span>
+						
+			<div className="bottom-link">
+				<Button variant="text">Sign In instead</Button>
+                <Button variant="contained">NEXT</Button>
+			</div>
+		</form>
+	    </div>
+		<div className="right">
+			<img src="../Sidelogo.PNG" alt="sidelogo"></img>
+			<p>One account. All of Fundoo working for you.</p>
+        </div>
+    </div>
+  )
 }
 
 export default Register
